1cdd48ea3ef27a38880c0d7c85524b998eb10b22
Terry Bu <ktbucifer@gmail.com>
2014-08-06 20:44:38 -0400

    got two different requests working but having trouble getting them in sync with each other on the template

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 var Youtube = require("youtube-api");
 var express = require("express");
+var myModule = require("./simple_http_request.js");
+var _ = require('underscore');
 
 Youtube.authenticate({
 	type: "key",
 	key: 'AIzaSyCdMoZiea0Z96EhH8cc3No7KJHv2rjey_c'
 });
 var content;
-var test;
+var real_content = [];
 Youtube.search.list({
     "part": "id, snippet",
     "q": 'Egg Fried Rice',
     "order": "viewCount",
-    "maxResults": 10,
+    "maxResults": 3,
     "chart": 'mostPopular'
 }, function (err, data) {
 	content = data.items;
-    console.log(content);
+	// console.log(_.isArray(content)); //true meaning that content is an ARRAY so underscore's each is not what's messing up the order for real_content
+	_.each(content, function(object) {
+		myModule.call_stats(object.id.videoId, function(data) {
+			real_content.push(data);
+		});	
+	});
 });
 
+
 var app = express();
+var helpers = require('express-helpers')(app);
+
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
@@ -26,8 +36,13 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.get("/", function(req, res) {
     res.render('template.ejs', {
     	title: "YouTube API Test Practice Node Express",
-    	content: content
+    	content: content,
+    	real_content: real_content
     });
+});
+
+app.get("/test", function(req, res) {
+    res.json(real_content);
 });
 
 app.listen(3000);
--- simple_http_request.js
+++ simple_http_request.js
@@ -21,26 +21,34 @@ var https = require('https');
 //   console.error(e);
 // });
 
-function call_stats_one_video (videoid) {
+function call_stats (videoid, callback) {
 	console.log("Calling Terry's call_stats_one_video function with video ID: " + videoid + "!");
 	var API_KEY = 'AIzaSyCdMoZiea0Z96EhH8cc3No7KJHv2rjey_c';
 	var options = {
 	host: 'www.googleapis.com',
 	path: '/youtube/v3/videos?part=id%2C+statistics%2C+snippet&id=' + videoid + '&fields=items(ageGating%2CcontentDetails%2CconversionPings%2CfileDetails%2Cid%2CliveStreamingDetails%2CmonetizationDetails%2Cplayer%2CprocessingDetails%2CprojectDetails%2CrecordingDetails%2Csnippet%2Cstatistics%2Cstatus%2Csuggestions%2CtopicDetails)&key=' + API_KEY,
 	method: 'GET'
-};
+	};
 	var req = https.request(options, function(res) {
 	  // console.log("statusCode: ", res.statusCode);
 	  // console.log("headers: ", res.headers);
-	  res.on('data', function(d) {
-	    process.stdout.write(d);
+	  var body = '';
+	  res.on('data', function(chunk) {
+	    //Do something here
+	    body += chunk;
+	  });
+	  res.on('end', function() {
+	  	var obj = JSON.parse(body);
+	  	callback(obj);
 	  });
 	});
 	req.end();
-
 	req.on('error', function(e) {
 	  console.error(e);
 	});
 }
 
-call_stats_one_video('pwbZEWlL8_Q');
+// call_stats('pwbZEWlL8_Q');
+
+//make that function exportable to other files (making it public)
+module.exports.call_stats = call_stats;
